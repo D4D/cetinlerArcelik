a1ee5f43b3a3861b3cef568bff0d968abff797d8
Cem Gencer <cem@d4d.com.tr>
2015-02-16 16:37:56 +0200

    pages

---
+++ google-play-services_lib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/d4d3/Work/prod/phoneGap/cetinler/Çetinler Arçelik/google-play-services_lib/ant-build/AndroidManifest.xml : \
+/Users/d4d3/Work/prod/phoneGap/cetinler/Çetinler Arçelik/google-play-services_lib/AndroidManifest.xml \
